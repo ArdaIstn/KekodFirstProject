7160a4f48c4d4fdfb2760865dd4a2cbe4ce5a6b1
Arda <arda_isitan55@hotmail.com>
2024-08-27 14:31:54 +0300

    Completed Bottom Nav and NavHost integration.

--- app/src/main/java/com/example/kekodfirstproject/MainActivity.kt
+++ app/src/main/java/com/example/kekodfirstproject/MainActivity.kt
@@ -1,20 +1,34 @@
 package com.example.kekodfirstproject
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.kekodfirstproject.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var bottomNav: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        initView()
+        eventView()
+
+        setContentView(binding.root)
+
+    }
+
+    private fun initView() {
+        navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        bottomNav = binding.bottomNavigation
+    }
+
+    private fun eventView() {
+        bottomNav.setupWithNavController(navHostFragment.navController)
     }
 }
